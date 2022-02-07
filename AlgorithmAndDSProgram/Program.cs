02a255dc048facae62f6a688aee101c1eca0a788
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-07 19:29:15 +0530

    [Raj] Added:- The welcome message in main program

---
+++ AlgorithmAndDSProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAndDSProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Displaying welcome message
+            Console.WriteLine("Welcome to the data structure and algorithm programs");
+            Console.ReadLine();
+        }
+    }
+}
