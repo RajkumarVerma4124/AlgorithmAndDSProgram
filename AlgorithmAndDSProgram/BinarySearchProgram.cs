706ed947a37226480dc3caec464cd579f9506063
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-07 19:45:22 +0530

    [Raj] Added:- The binary search program for searching words

---
+++ AlgorithmAndDSProgram/BinarySearchProgram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlgorithmAndDSProgram
+{
+    public class BinarySearchProgram
+    {
+        public static void BinarySearchWord()
+        {
+            Console.WriteLine("Binary Search Program To Search Words From File\n");
+            string[] words = null;
+            Console.WriteLine("Reading words from the file");
+            string filePath = @"E:\CODING\Coding\React Web Apps\coreAPI\Fellowship\AlgorithmAndDSProgram\AlgorithmAndDSProgram\RandomText.txt";
+            if (File.Exists(filePath))
+            {
+                words = File.ReadAllText(filePath).Split(',');
+            }
+            Console.WriteLine();
+
+            //Sorting the words list
+            Array.Sort(words);
+
+            Console.Write("After sorting the list : ");
+            foreach (string word in words)
+            {
+                Console.Write(word.Trim() + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Enter a word to search : ");
+            string search = Console.ReadLine();
+            bool result = Perform.BinarySearch(words, search);
+            if (result)
+                Console.WriteLine("The word {0} is present in the list", search);
+            else
+                Console.WriteLine("The word {0} is not present in the list", search);
+            Console.ReadLine();
+        }
+    }
+}
--- AlgorithmAndDSProgram/Program.cs
+++ AlgorithmAndDSProgram/Program.cs
@@ -14,8 +14,29 @@ namespace AlgorithmAndDSProgram
             Console.WriteLine("Welcome to the data structure and algorithm programs");
             Console.ReadLine();
 
-            //Calling the string permutation method
-            PermutationProgram.StringPermuation();
+            while (true)
+            {
+                Console.WriteLine("1: String Permutation \n2: Binary String Search \n3: Exit");
+                Console.Write("Enter a choice from above : ");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        //Calling the string permutation method
+                        PermutationProgram.StringPermuation();
+                        break;
+                    case 2:
+                        //Calling the binary search string method
+                        BinarySearchProgram.BinarySearchWord();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice");
+                        continue;
+                }
+            }
         }
     }
 }
