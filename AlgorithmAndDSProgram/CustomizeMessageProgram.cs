7a1bd5817fc83749bb5173d3c59f3a99bec0bd96
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-08 10:55:59 +0530

    [Raj] Added:- The code to take current date from system

--- AlgorithmAndDSProgram/CustomizeMessageProgram.cs
+++ AlgorithmAndDSProgram/CustomizeMessageProgram.cs
@@ -11,13 +11,13 @@ namespace AlgorithmAndDSProgram
         public static void PrintMessage()
         {
             RegexPattern regexPattern = new RegexPattern();
-            /*if(regexPattern.FirstName() == true)
+            if(regexPattern.FirstName() == true)
                 Console.WriteLine("The first name is valid");
             if (regexPattern.LastName() == true)
                 Console.WriteLine("The last name is valid"); 
             if (regexPattern.PhoneNumber() == true)
-                Console.WriteLine("The phone number is valid");*/
-            if (regexPattern.DateTime() == true)
+                Console.WriteLine("The phone number is valid");
+            if (regexPattern.CurrentDate() == true)
                 Console.WriteLine("The date time is valid");
             regexPattern.Message();
         }
--- AlgorithmAndDSProgram/RegexPattern.cs
+++ AlgorithmAndDSProgram/RegexPattern.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
-
+using System.Globalization;
 
 namespace AlgorithmAndDSProgram
 {
@@ -15,7 +15,8 @@ namespace AlgorithmAndDSProgram
         public readonly Regex lastName = new Regex(@"(^[A-Z]{1}[a-z]{2,}$)");
         public readonly Regex phoneNum = new Regex(@"(^91[ ][0-9]{10}$)");
         public readonly Regex dateTime = new Regex(@"(^(((0[1-9]|[12][0-9]|30)[-/]?(0[13-9]|1[012])|31[-/]?(0[13578]|1[02])|(0[1-9]|1[0-9]|2[0-8])[-/]?02)[-/]?[0-9]{4}|29[-/]?02[-/]?([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00))$)");
-        public string fName, lName, phnNum, dtTime;
+        public string fName, lName, phnNum, todaysDate;
+        public readonly DateTime date = DateTime.Today;
 
         //Constructor to initialize values
         public RegexPattern()
@@ -23,7 +24,7 @@ namespace AlgorithmAndDSProgram
             this.fName = null;
             this.lName = null;
             this.phnNum = null;
-            this.dtTime = null;
+            this.todaysDate = null;
         }
 
         //Method to check and take the first name format
@@ -72,16 +73,17 @@ namespace AlgorithmAndDSProgram
         }
 
         //Method to check and take the date time format
-        public bool DateTime()
+        public bool CurrentDate()
         {
-            Console.Write("Enter the date and time in dd/mm/yyyy format : ");
-            dtTime = Console.ReadLine();
-            if (dateTime.IsMatch(dtTime))
+            Console.Write("Entering the current date in dd/mm/yyyy format from system : ");
+            todaysDate = date.ToString("dd/MM/yyyy");
+            Console.WriteLine(todaysDate);
+            if (dateTime.IsMatch(todaysDate))
                 return true;
             else
             {
                 Console.WriteLine("The given date time format is not valid");
-                DateTime();
+                CurrentDate();
             }
             return default;
         }
@@ -90,7 +92,7 @@ namespace AlgorithmAndDSProgram
         public void Message()
         {
             string fullName = fName+" "+lName;
-            Console.WriteLine($"Hello {fName}, We have your full name as {fullName} in our system.\nYour contact number is {phnNum}. Please, let us know in case of any clarification.\nThank you BridgeLabz {dtTime}.");
+            Console.WriteLine($"Hello {fName}, We have your full name as {fullName} in our system.\nYour contact number is {phnNum}. Please, let us know in case of any clarification.\nThank you BridgeLabz {todaysDate}.");
         }
     }
 }
