01673199e5d1474245fc7b247542082d4bf08c0f
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-12 16:37:34 +0530

    [Raj] Refactor:- The code to print prime anagaram using stack & queue linklist

--- AlgorithmAndDSProgram/DataStructrures/QueueLinklist.cs
+++ AlgorithmAndDSProgram/DataStructrures/QueueLinklist.cs
@@ -5,22 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace AlgorithmAndDSProgram.DataStructrures
-{ 
-    public class StackLinkList<G> where G : IComparable
+{
+    public class QueueLinklist<G> where G : IComparable
     {
         //Creating the linlist object 
         UnorderedList<G> list = new UnorderedList<G>();
 
         //Creating add first method to add the element at first position i.e head
-        public void Push(G data)
+        public void Enqueue(G data)
         {
-            list.AddFirst(data);
+            list.AddLast(data);
         }
 
         //Displaying the data from linklist nodes
         public void Display()
         {
-           list.Display();
+            list.Display();
         }
     }
 }
